99e9e9c0986279440b2c6a12606f6354221f271b
chibinz <chibinzhang@hotmail.com>
2019-08-16 13:56:48 +0800

    Finish disassembling data processing, multiply, bx, swap of ARM opcodes
    Encoding of ARM opcodes is very confusing. Many opcodes with very different functionalities are encoded in a similar pattern, making it hard to decode.

--- src/main.rs
+++ src/main.rs
@@ -25,48 +25,51 @@ fn main()
 
     let mut i = 0;
 
-    // let u: u16 = 0b10000011;
-    // let v = u as i8;
-    // println!("{}", v);
+    let opcodes = 0b10101010;
+    let bit = |start: u32, end: u32| 
+    {
+        opcodes >> end & (1 << start - end + 1) - 1
+    };
+
+    assert_eq!(bit(7, 0), 0b10101010);
+
 
     while i < file.len()
     {
         // print!("{:02x} ", file[i]);
         // i += 1;
 
-        // if i % 0x100 == 0 && i > 0
-        // {
-        //     println!("{}th byte, press any key to continue", i);
-        //     let mut input = String::new();
-        //     io::stdin().read_line(&mut input).unwrap();
-        // }
+        if i % 0x100 == 0 && i > 0
+        {
+            println!("{}th byte, press any key to continue", i);
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).unwrap();
+        }
 
         // match cpu0.mode
         // {
         //     cpu::Mode::ARM => 
         //     {
-        //         let word: u32 = ((file[i] as u32)) + 
-        //                         ((file[i + 1] as u32) << 8) + 
-        //                         ((file[i + 2] as u32) << 16) + 
-        //                         ((file[i + 3] as u32) << 24); 
+                let word: u32 = ((file[i] as u32)) + 
+                                ((file[i + 1] as u32) << 8) + 
+                                ((file[i + 2] as u32) << 16) + 
+                                ((file[i + 3] as u32) << 24); 
                 
-        //         // print!("{:08x}: {:08x} | {:032b} ", i, word, word);
-        //         if arm::disassemble(word) == 1
-        //         {
-        //             cpu0.mode = cpu::Mode::THUMB;
-        //         }
-        //         // print!("\n");
+                print!("{:08x}: {:08x} | {:032b} ", i, word, word);
+                print!("{}", arm::disassemble(word));
+                print!("\n");
                 
-        //         i += 4;
+                i += 4;
         //     },
         //     cpu::Mode::THUMB =>
         //     {
-                let halfword: u16 = (file[i] as u16) + ((file[i + 1] as u16) << 8);
+                // let halfword: u16 = (file[i] as u16) + ((file[i + 1] as u16) << 8);
 
                 // print!("{:08x}: {:04x} | {:016b} ", i, halfword, halfword);
+                // thumb::disassemble(halfword);
                 // print!("{}\n", thumb::disassemble(halfword));
 
-                i += 2;
+                // i += 2;
         //     }
         // };
     }
