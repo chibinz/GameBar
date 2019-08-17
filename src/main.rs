c6f1f896e2bca07f56c1755f4a6de7f4583e84b3
chibinz <chibinzhang@hotmail.com>
2019-08-17 18:58:04 +0800

    Finish disassembling halfword, signed and single data transfer of ARM opcodes
    The arm disassemble() function has grown a lot in size with readability reduced. Only the block data transfer remains to be implemented. Coprocessor related opcodes are ignored for the time being.

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,9 @@ pub mod memory;
 
 use std::{env, fs, io};
 use std::time::Instant;
-use cpu::{register::Register, arm, thumb};
 
 fn main() 
 {
-    let start = Instant::now();
-
     let args: Vec<String> = env::args().collect();
 
     if args.len() == 1
@@ -22,37 +19,26 @@ fn main()
 
     let file = fs::read(args[1].to_string()).unwrap();
     println!("rom size: 0x{:08x} bytes", file.len());
-
-    // let opcodes = 0b10101010;
-    // let bit = |start: u32, end: u32| 
-    // {
-    //     opcodes >> end & (1 << start - end + 1) - 1
-    // };
-    // assert_eq!(bit(7, 0), 0b10101010);
-
+    
+    let start = Instant::now();
     cpu.register.r[15] = 0x08000000;
     while (cpu.register.r[15] as usize) - 0x08000000 < file.len()
     {
-        // print!("{:02x} ", file[i]);
-        // i += 1;
-
-        if cpu.register.r[15] % 0x100 == 0
-        {
-            println!("{:08x}th byte, press any key to continue", cpu.register.r[15]);
-            let mut input = String::new();
-            io::stdin().read_line(&mut input).unwrap();
-        }
+        // if cpu.register.r[15] % 0x100 == 0
+        // {
+        //     println!("{:08x}th byte, press any key to continue", cpu.register.r[15]);
+        //     let mut input = String::new();
+        //     io::stdin().read_line(&mut input).unwrap();
+        // }
         
         let word = memory.load32(cpu.register.r[15]);
         print!("{:08x}: {:08x} | {:032b} ", cpu.register.r[15], word, word);
         println!("{}", cpu::arm::disassemble(word));
 
-
         // let halfword: u16 = (file[i] as u16) + ((file[i + 1] as u16) << 8);
         // print!("{:08x}: {:04x} | {:016b} ", i, halfword, halfword);
         // thumb::disassemble(halfword);
         // print!("{}\n", thumb::disassemble(halfword));
-
 
         cpu.register.r[15] += 4;
     }
