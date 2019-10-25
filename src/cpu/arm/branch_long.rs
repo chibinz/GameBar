3a0f25e51a76e9bd6a23c061df56e6bbaf586316
chibinz <chibinzhang@hotmail.com>
2019-10-25 16:48:03 +0800

    Implement branch exchange/long instructions.

---
+++ src/cpu/arm/branch_long.rs
@@ -0,0 +1,19 @@
+use util::*;
+use cpu::CPU;
+
+pub fn decode_execute(cpu: &mut CPU, instruction: u32)
+{
+    // It the link bit is set, the old value of pc is written
+    // to the link register, which is R14.
+    let l = bit(instruction, 24);
+
+    // The offset is left shifted by 2 and sign extended to 32 bits
+    let offset: i32 = sign_extend(bits(instruction, 23, 0) << 2, 25);
+    
+    if l
+    {
+        cpu.register.r[14] = cpu.register.r[15];
+    }
+
+    cpu.register.r[15] += offset;
+}
