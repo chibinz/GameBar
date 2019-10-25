98cbe1d4182c2ce0db2f48061917d3610baa3d81
chibinz <chibinzhang@hotmail.com>
2019-10-25 15:51:29 +0800

    Implement PSR transfer instructions

--- src/cpu/arm/mod.rs
+++ src/cpu/arm/mod.rs
@@ -1,3 +1,30 @@
 pub mod disassemble;
-pub mod execute;
 pub mod data_processing;
+pub mod psr_transfer;
+
+use crate::cpu::CPU;
+
+impl CPU
+{
+    /// Execute instruction
+    pub fn execute(&mut self, instruction: u32) -> u32
+    {
+        let cond = instruction >> 28 & 0b1111;
+        if self.check_condition(cond)
+        {
+            data_processing::execute(self, data_processing::decode(instruction));
+        }
+        
+        self.register.r[15] += 4;
+
+        return 0;
+    }
+
+}
+
+#[cfg(test)]
+mod tests
+{
+    use super::*;
+
+}
--- src/cpu/register.rs
+++ src/cpu/register.rs
@@ -48,7 +48,32 @@ impl Register
         }
     }
 
-    #[inline(always)]
+    #[inline]
+    pub fn get_cpsr(&self) -> u32
+    {
+        self.cpsr
+    }
+
+    /// Set defined bits of CPSR.
+    /// If parameter f is set, transfer only the flag bits.
+    /// Reserved bits of CPSR are kept intact.
+    #[inline]
+    pub fn set_cpsr(&mut self, r: u32, f: bool)
+    {
+        let mask = if f {0xf0000000} else {0xf00000ff};
+        
+        self.cpsr &= !mask;
+        self.cpsr |= r & mask;
+    }
+
+    /// Copy SPSR of current mode to CPSR
+    #[inline]
+    pub fn restore_cpsr(&mut self)
+    {
+        self.cpsr = self.spsr[self.get_cpsr_mode() as usize];
+    }
+
+    #[inline]
     pub fn get_cpsr_bit(&self, bit: PSRBit) -> bool
     {
         self.cpsr >> (bit as u32) & 1 == 1
@@ -70,15 +95,17 @@ impl Register
     #[inline(always)]
     pub fn get_cpsr_mode(&self) -> PSRMode
     {
-        match self.cpsr
+        use PSRMode::*;
+
+        match self.cpsr & 0b11111
         {
-            0b10000 => PSRMode::User,
-            0b10001 => PSRMode::FIQ,
-            0b10010 => PSRMode::IRQ,
-            0b10011 => PSRMode::Supervisor,
-            0b10111 => PSRMode::Abort,
-            0b11011 => PSRMode::Undefined,
-            0b11111 => PSRMode::System, 
+            0b10000 => User,
+            0b10001 => FIQ,
+            0b10010 => IRQ,
+            0b10011 => Supervisor,
+            0b10111 => Abort,
+            0b11011 => Undefined,
+            0b11111 => System, 
             _       => panic!("Invalid PSR Mode\n")
         }
     }
@@ -91,9 +118,41 @@ impl Register
         self.cpsr |= m as u32;
     }
 
-    pub fn restore_cpsr(&mut self)
+    /// Get SPSR of current mode
+    #[inline]
+    pub fn get_spsr(&mut self) -> u32
     {
-        self.cpsr = self.spsr[self.get_cpsr_mode() as usize];
+        self.spsr[self.get_spsr_index()]
+    }
+
+    /// Set defined bits of SPSR of current mode.
+    /// If parameter f is set, transfer only the flag bits.
+    /// Reserved bits of SPSR are kept intact.
+    #[inline]
+    pub fn set_spsr(&mut self, r: u32, f: bool)
+    {
+        let mask = if f {0xf0000000} else {0xf00000ff};
+        
+        self.spsr[self.get_spsr_index()] &= !mask;
+        self.spsr[self.get_spsr_index()] |= r & mask;
+    }    
+    
+    /// Because SPSR are banked and stored as array,
+    /// get the index of current SPSR.
+    #[inline]
+    fn get_spsr_index(&self) -> usize
+    {
+        use PSRMode::*;
+
+        match self.get_cpsr_mode()
+        {
+            FIQ        => 0,
+            Supervisor => 1,
+            Abort      => 2,
+            IRQ        => 3,
+            Undefined  => 4,
+            _          => panic!("Curent mode does not have a SPSR"),
+        }
     }
 }
 
