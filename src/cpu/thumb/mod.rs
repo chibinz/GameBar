c0b4998c718856683d702cb119cd68239ba85655
chibinz <chibinzhang@hotmail.com>
2020-01-10 22:16:34 +0800

    Implement add subtract for thumb, add unit tests

--- src/cpu/thumb/mod.rs
+++ src/cpu/thumb/mod.rs
@@ -1,3 +1,37 @@
 pub mod disassemble;
 pub mod instruction;
 pub mod move_shifted_register;
+pub mod add_subtract;
+
+use crate::util::*;
+use crate::cpu::CPU;
+use crate::memory::Memory;
+
+/// Execute instruction
+pub fn execute(cpu: &mut CPU, memory: &mut Memory) -> u32
+{
+    cpu.register.r[15] += 2;
+
+    dispatch(cpu, memory, cpu.ir);
+
+    0
+}
+
+pub fn fetch(cpu: &mut CPU, memory: &mut Memory)
+{
+    if cpu.flushed
+    {
+        cpu.ir = memory.load32(cpu.register.r[15]);
+        cpu.register.r[15] += 2;
+        cpu.flushed = false;
+    }
+    else
+    {
+        cpu.ir = memory.load32(cpu.register.r[15] - 2);
+    }
+}
+
+pub fn dispatch(cpu: &mut CPU, memory: &mut Memory, instruction: u32)
+{
+
+}
