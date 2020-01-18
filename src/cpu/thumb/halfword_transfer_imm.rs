5a367587d6736e78006bf600bc8445814c762ce7
chibinz <chibinzhang@hotmail.com>
2020-01-18 22:23:42 +0800

    Implement single and halfword data transfer with immediate offset

---
+++ src/cpu/thumb/halfword_transfer_imm.rs
@@ -0,0 +1,37 @@
+use crate::util::*;
+use crate::cpu::CPU;
+use crate::memory::Memory;
+
+#[inline]
+pub fn decode_execute(cpu: &mut CPU, memory: &mut Memory, instruction: u16)
+{
+    execute(cpu, memory, decode(instruction));
+}
+
+#[inline]
+fn decode(instruction: u16) -> (bool, u32, u32, u32)
+{
+    // Single and halfword data transfer use similar encoding format.
+    // Thus is handled together.
+    let l = instruction.bit(11);
+    let offset5 = instruction.bits(10, 6);
+    let rb = instruction.bits(5, 3);
+    let rd = instruction.bits(2, 0);
+
+    (l, offset5, rb, rd)
+}
+ 
+#[inline]
+fn execute(cpu: &mut CPU, memory: &mut Memory, (l, offset5, rb, rd): (bool, u32, u32, u32))
+{   
+    let address = cpu.r[rb as usize] + (offset5 << 1);
+
+    if l
+    {
+        cpu.r[rd as usize] = memory.load16(address) as u32;
+    }
+    else
+    {
+        memory.store16(address, cpu.r[rd as usize] as u16);
+    }
+}
---
+++ src/cpu/thumb/single_transfer_imm.rs
@@ -0,0 +1,37 @@
+use crate::util::*;
+use crate::cpu::CPU;
+use crate::memory::Memory;
+
+#[inline]
+pub fn decode_execute(cpu: &mut CPU, memory: &mut Memory, instruction: u16)
+{
+    execute(cpu, memory, decode(instruction));
+}
+
+#[inline]
+fn decode(instruction: u16) -> (u32, u32, u32, u32)
+{
+    // Single and halfword data transfer use similar encoding format.
+    // Thus is handled together.
+    let bl = instruction.bits(12, 11);
+    let offset5 = instruction.bits(10, 6);
+    let rb = instruction.bits(5, 3);
+    let rd = instruction.bits(2, 0);
+
+    (bl, offset5, rb, rd)
+}
+ 
+#[inline]
+fn execute(cpu: &mut CPU, memory: &mut Memory, (bl, offset5, rb, rd): (u32, u32, u32, u32))
+{   
+    let base = cpu.r[rb as usize] + offset5;
+
+    match bl
+    {
+        0b00 => memory.store32(base + (offset5 << 2), cpu.r[rd as usize]),
+        0b01 => memory.store8(base + offset5, cpu.r[rd as usize] as u8),
+        0b10 => cpu.r[rd as usize] = memory.load32(base + (offset5 << 2)),
+        0b11 => cpu.r[rd as usize] = memory.load8(base + offset5) as u32,
+        _    => unreachable!(),
+    }
+}
