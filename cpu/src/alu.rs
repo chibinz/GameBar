6d06f2c74f7dab4ad2fb37dbac470908ef834821
Chibin Zhang <chibinzhang@hotmail.com>
2021-02-10 19:49:06 +0800

    Refactor flag setting in alu

--- cpu/src/alu.rs
+++ cpu/src/alu.rs
@@ -26,26 +26,6 @@ pub fn with_flags(result: u32, c: bool, v: bool) -> (u32, Flags) {
     (result, (result.bit(31), result == 0, c, v))
 }
 
-#[inline]
-fn add_carry(op1: u32, op2: u32) -> bool {
-    op1.overflowing_add(op2).1
-}
-
-#[inline]
-fn add_overflow(op1: u32, op2: u32) -> bool {
-    (op1 as i32).overflowing_add(op2 as i32).1
-}
-
-#[inline]
-fn sub_carry<T: PartialOrd>(op1: T, op2: T) -> bool {
-    op1 >= op2
-}
-
-#[inline]
-fn sub_overflow(op1: u32, op2: u32) -> bool {
-    (op1 as i32).overflowing_sub(op2 as i32).1
-}
-
 // Logical operations
 
 #[inline]
@@ -94,19 +74,19 @@ pub fn teq(op1: u32, op2: u32, c: bool, v: bool) -> (u32, Flags) {
 pub fn add(op1: u32, op2: u32, _c: bool, _v: bool) -> (u32, Flags) {
     with_flags(
         op1.wrapping_add(op2),
-        add_carry(op1, op2),
-        add_overflow(op1, op2),
+        op1.overflowing_add(op2).1,
+        (op1 as i32).overflowing_add(op2 as i32).1,
     )
 }
 
 #[inline]
 pub fn adc(op1: u32, op2: u32, c: bool, _v: bool) -> (u32, Flags) {
-    let (opc, carry2) = op2.overflowing_add(c as u32);
-    let result = op1.wrapping_add(opc);
+    let (opc, c1) = op2.overflowing_add(c as u32);
+    let (result, c2) = op1.overflowing_add(opc);
 
     with_flags(
         result,
-        add_carry(op1, opc) || carry2,
+         c1 || c2,
         ((!op1 ^ op2) & (op1 ^ result)).bit(31),
     )
 }
@@ -115,8 +95,8 @@ pub fn adc(op1: u32, op2: u32, c: bool, _v: bool) -> (u32, Flags) {
 pub fn sub(op1: u32, op2: u32, _c: bool, _v: bool) -> (u32, Flags) {
     with_flags(
         op1.wrapping_sub(op2),
-        sub_carry(op1, op2),
-        sub_overflow(op1, op2),
+        op1 >= op2,
+        (op1 as i32).overflowing_sub(op2 as i32).1,
     )
 }
 
